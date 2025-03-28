b1ea3c2573f46dcac612c4db0f265aa73db0b626
Bardi AB <bardiab@Bardi-2.local>
2025-03-28 02:00:48 -0400

    linking

--- src/components/ConCard.jsx
+++ src/components/ConCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export const ConCard = ({className, title,b1, b2}) => {
+export const ConCard = ({className, title,b1,h1, b2}) => {
   return (
     <div className={`flex justify-center items-center pt-5 ${className} lgMobile:flex-col mdMobile:flex-col sMobile:flex-col`}>
     {/* <button className={`flex justify-center items-center px-10 py-4 w-full max-h-16 text-xl text-center rounded-sm hover:scale-95 hover:bg-opacity-90 ${b1}`}>
       405-595-7072
     </button> */}
-    {title === "Contact"?  "" : <Link to='/#contact' className={`flex justify-center items-center px-10 py-4 m-5 w-full max-h-16 text-xl rounded-sm border-2 hover:scale-95 ${b2}`}>
+    {title === "Contact"?  "" : <Link to={`/contact#contact`} className={`flex justify-center items-center px-10 py-4 m-5 w-full max-h-16 text-xl rounded-sm border-2 hover:scale-95 ${b2}`}>
       Contact Us
     </Link> }
   </div>
